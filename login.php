9586182be776444c8980d7befd75959a7866b053
Benjihunt97 <73445088+Benjihunt97@users.noreply.github.com>
2023-12-31 23:17:18 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve user data from the request
     $data = json_decode(file_get_contents('php://input'), true);
@@ -7,20 +8,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = filter_var($data['email'], FILTER_VALIDATE_EMAIL);
     $password = htmlspecialchars($data['password']);
 
-    // Simulate user authentication (replace this with your actual authentication logic)
-    // For simplicity, this example checks for a specific email and password
-    $validUser = ($email === 'user@example.com' && $password === 'password');
+    // Simulate user registration (replace this with your actual registration logic)
+    // For simplicity, this example stores user data in a text file
+    $registrationSuccessful = registerUser($email, $password);
 
-    if ($validUser) {
+    if ($registrationSuccessful) {
+        // Send email confirmation
+        sendConfirmationEmail($email);
+
         // Respond to the client (you may want to customize this based on your needs)
-        echo json_encode(['message' => 'Login successful']);
+        echo json_encode(['message' => 'Registration successful']);
     } else {
         // Respond to the client with an error message (you may want to customize this based on your needs)
-        echo json_encode(['error' => 'Invalid email or password']);
+        echo json_encode(['error' => 'Registration failed']);
     }
 } else {
     // Handle invalid requests
     http_response_code(405);
     echo json_encode(['error' => 'Method Not Allowed']);
 }
+
+function registerUser($email, $password) {
+    // Simulate storing user data in a text file
+    $userData = [
+        'email' => $email,
+        'password' => password_hash($password, PASSWORD_DEFAULT), // Hash the password
+    ];
+
+    $usersFile = 'users.txt';
+    $userString = json_encode($userData) . PHP_EOL; // Add a new line for each user
+
+    // Append user data to the file
+    return file_put_contents($usersFile, $userString, FILE_APPEND | LOCK_EX) !== false;
+}
+
+function sendConfirmationEmail($email) {
+    // You need to customize the email content and headers
+    $to = 'drbenjaminhunt@gmail.com;
+    $subject = 'Registration Confirmation';
+    $message = 'Thank you for registering!';  // Customize this message
+    $headers = 'From: webmaster@example.com' . "\r\n" .
+               'Reply-To: webmaster@example.com' . "\r\n" .
+               'X-Mailer: PHP/' . phpversion();
+
+    // Use the mail function to send the email
+    mail($to, $subject, $message, $headers);
+}
+
 ?>
